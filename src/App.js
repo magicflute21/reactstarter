925e6b1fdeb2ce38b3febfbd469b60c77d7244b5
Triin Lemba <triin.lemba@advectus.net>
2021-10-29 20:48:53 +0300

    App class component to functional

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React, { Component} from "react";
 import '/dist/style/tailwind/tailwindBuild.css';
-import { PaginationTable } from './components/PaginationTable';
 import {hot} from "react-hot-loader";
 
-class App extends Component{
-  render(){
-    return(
-      <div className="App">
-        <PaginationTable />
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div className="App">
+      <h1>hello</h1>
+    </div>
+  )
 }
 
 export default hot(module) (App);
